ca1690e97dbf74c3f395b9426fdd8a3d21888891
Dhruvil Thummar <dhruvilthummar2007@gmail.com>
2026-02-04 17:34:03 +0530

    feat: Add dashboard templates for superadmin, college, hod, student, and parent, including navigation and footer components

--- attendance_system/app.py
+++ attendance_system/app.py
@@ -23,5 +23,25 @@ def login():
 def register():    
     return render_template("register.html")
 
+@app.route("/superadmindashboard")
+def dashboard():    
+    return render_template("superadmin/sudashbord.html")
+
+@app.route("/collegedashboard")
+def cdashboard():    
+    return render_template("college/cdashbord.html")
+
+@app.route("/hoddashboard")
+def hdashboard():    
+    return render_template("hod/hdashbord.html")
+
+@app.route("/studentdashboard")
+def sdashboard():    
+    return render_template("student/sdashbord.html")
+
+@app.route("/parentdashboard")
+def pdashboard():    
+    return render_template("parent/pdashbord.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
